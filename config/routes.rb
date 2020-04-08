a0942d4af7aaaa1078361732c6d08fdb642fb2c9
NYuya <noooochan5@gmail.com>
2020-04-08 22:54:39 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+
+  resources :books do
+  	resource :favorites, only: [:create, :destroy]
+  	resource :book_comments, only: [:create, :destroy]
+  end
+
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :relationships, only: [:create, :destroy]
+
+  get 'followed/:id' => 'relationships#followed', as: 'followed'
+  get 'follower/:id' => 'relationships#follower', as: 'follower'
+
+  root 'home#top'
+  get "home/about" => "home#about"
+
+end
